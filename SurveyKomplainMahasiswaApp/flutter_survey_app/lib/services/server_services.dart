3a368c5d644cea213b56a1d0a3b7822ccb83274f
I <91296724+i9191@users.noreply.github.com>
2023-10-23 19:05:35 +0700

    Ubah dio ke http

--- SurveyKomplainMahasiswaApp/flutter_survey_app/lib/services/server_services.dart
+++ SurveyKomplainMahasiswaApp/flutter_survey_app/lib/services/server_services.dart
@@ -1,20 +1,30 @@
-import 'package:dio/dio.dart';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter_survey_app/models/survey.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ServerService {
-  final dio = Dio();
-  
-  Future<List> getAllData() async{
+  Future<List<Survey>> fetchSurveys() async {
     await dotenv.load(fileName: ".env");
-    final String? baseUrl = dotenv.env['SERVER_ADDRESS'];
-    Response resp;
-    final String uri = baseUrl! + 'all_data';
-    print(uri);
-
-    resp = await dio.post(uri);
-    if(resp.statusCode == 200){
-      return resp.data;
-    }else{
+    final String? baseUrl = dotenv.env['SERVER_ADDRESS']! + 'all_data';
+    try {
+      http.Response response =
+          await http.post(Uri.parse(baseUrl!));
+      if (response.statusCode == HttpStatus.ok) {
+        print('s');
+        List<Survey> surveys = [];
+        for (var item in jsonDecode(response.body)) {
+          print(item);
+          surveys.add(Survey.fromJson(item));
+        }
+        return surveys;
+      } else {
+        throw Exception('Failed to load surveys');
+      }
+    } catch (e) {
+      print(e);
       return [];
     }
   }
