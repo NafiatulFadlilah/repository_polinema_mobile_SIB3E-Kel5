f0213728fb0f6856de3d04e60fa5d34c576d4ea3
Nafiatul Fadlilah <nfdnafiatulfa@gmail.com>
2023-11-07 08:29:03 +0700

    warna appbar

--- SurveyKomplainMahasiswaApp/flutter_survey_app/lib/main.dart
+++ SurveyKomplainMahasiswaApp/flutter_survey_app/lib/main.dart
@@ -7,15 +7,47 @@ void main() {
   runApp(const MainApp());
 }
 
+// class MainApp extends StatelessWidget {
+//   const MainApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: MyApp(),
+//     );
+//   }
+// }
+
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    const primaryColor = Color.fromARGB(255, 0, 88, 160);
+    final MaterialColor primarySwatch = MaterialColor(
+      primaryColor.value,
+      <int, Color>{
+        50: primaryColor.withOpacity(0.1),
+        100: primaryColor.withOpacity(0.2),
+        200: primaryColor.withOpacity(0.3),
+        300: primaryColor.withOpacity(0.4),
+        400: primaryColor.withOpacity(0.5),
+        500: primaryColor,
+        600: primaryColor.withOpacity(0.7),
+        700: primaryColor.withOpacity(0.8),
+        800: primaryColor.withOpacity(0.9),
+        900: primaryColor.withOpacity(1.0),
+      },
+    );
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: primarySwatch,
       ),
       home: MyApp(),
     );
