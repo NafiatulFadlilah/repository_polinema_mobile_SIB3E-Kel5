b34b89ae5606595a9cda43c7b6ee696cfbff8823
I <91296724+i9191@users.noreply.github.com>
2023-12-02 14:58:55 +0700

    add status and seed mahasiswa csv

--- SurveyKomplainMahasiswaApp/laravel-backend_survey-api/database/seeders/StudentsSeeder.php
+++ SurveyKomplainMahasiswaApp/laravel-backend_survey-api/database/seeders/StudentsSeeder.php
@@ -45,6 +45,22 @@ class StudentsSeeder extends Seeder
             'phone' => '089680536891',
             'password' => '2141762140',
         ]);
+        
+        $csvFile = fopen(base_path("database/data/StudentData.csv"), "r");
+  
+        $firstline = true;
+        while (($data = fgetcsv($csvFile, 300, ",")) !== FALSE) {
+            if (!$firstline) {
+                StudentData::create([
+                    'nim' => $data['0'],
+                    'name' => $data['1'],
+                    'status' => $data['2'],
+                    'phone' => $data['3'],
+                    'password' => $data['0'],
+                ]);    
+            }
+            $firstline = false;
+        }
         Schema::enableForeignKeyConstraints();
     }
 }
